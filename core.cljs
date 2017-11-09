be44c27b19b089f420884d9083e3b129d7cb11a9
apeckham <apeckham@gmail.com>
2017-11-08 19:26:47 -0800

    placeholders

--- core.cljs
+++ core.cljs
@@ -4,6 +4,6 @@
       client (new (.-Client pg) url)]
   (-> (.connect client)
       (.then (fn []
-               (-> (.query client "SELECT NOW()")
+               (-> (.query client "SELECT $1::text as now" #js ["hi world"])
                    (.then #(js/console.log (-> % .-rows first .-now))))))
       (.then #(.end client))))
